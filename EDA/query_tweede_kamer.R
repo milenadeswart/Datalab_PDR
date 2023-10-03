3a29e16bc237b7a54395097a19d8d4d22462a4b4
Coen Eisma <c.w.eisma@minocw.nl>
2023-10-03 10:55:51 +0200

    Downloaden van alle relevante documenten en dat is gelukt.

--- EDA/query_tweede_kamer.R
+++ EDA/query_tweede_kamer.R
@@ -28,15 +28,16 @@ inlezen_pdf <- function(url){
 }
 
 
-# filteren op documenten in 2022 van regering
+# filteren op documenten in 2022 van regering + kernwoorden OCW, eerste 100
 url_docs_20_nu <- "https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/Document?$filter=year(DatumOntvangst) ge 2020 and Soort eq 'Brief regering'"
 docs_20_nu <- get_odata(url_docs_20_nu)
-docs_20_nu_onderwijs <- docs_20_nu %>%
+docs_20_nu_ocw <- docs_20_nu %>%
   filter(if_any(everything(), ~ str_detect(., "Onderwijs|onderwijs|Cultuur|cultuur|Wetenschap|wetenschap"))) %>%
   filter(ContentType == "application/pdf")
 
 
-test <- docs_20_nu_onderwijs %>%
+# TEst op de eerste 100 documenten
+test <- docs_20_nu_ocw %>%
   head(100)
 test_txt <- test %>%
   mutate(tekst = map(url_document, inlezen_pdf)) %>%
@@ -47,3 +48,15 @@ test_txt <- test %>%
   dplyr::ungroup() %>%
   select(Id, pagina, everything())
 
+
+# Poging op alle 1173 documenten
+docs_20_nu_ocw_txt <- docs_20_nu_ocw %>%
+  mutate(tekst = map(url_document, inlezen_pdf)) %>%
+  # tidyr::unnest_wider(tekst) %>%
+  tidyr::unnest_longer(tekst) %>%
+  dplyr::group_by(Id) %>%
+  dplyr::mutate(pagina = row_number()) %>%
+  dplyr::ungroup() %>%
+  select(Id, pagina, everything())
+
+write_rds(docs_20_nu_ocw_txt, path = "datasets/OCW_documenten.RDS")
